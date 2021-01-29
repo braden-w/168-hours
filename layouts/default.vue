778e158c39550d5103e023049bec25b58ea28d3b
Braden <bmw02002turbo@gmail.com>
2021-01-28 20:21:02 -0800

    Removed clip button

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,9 +27,6 @@
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
 
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
       <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
