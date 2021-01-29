ae5add0ce15b4fb1de690d61d84b0cdbc1c4ed7f
Braden <bmw02002turbo@gmail.com>
2021-01-28 20:22:25 -0800

    Removed fixed bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,10 +26,6 @@
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
