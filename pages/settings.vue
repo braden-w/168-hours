7b4c310158c491a62374f0e8a11134d80adaffa9
Braden <bmw02002turbo@gmail.com>
2021-01-29 15:19:56 -0800

    feat: Start Timer button

--- pages/settings.vue
+++ pages/settings.vue
@@ -3,7 +3,7 @@
     <v-card>
       <v-card-title>Settings</v-card-title>
       <v-card-actions>
-        <v-btn color="success">Start timer</v-btn>
+        <v-btn color="success" @click="startTimer">Start timer</v-btn>
         <v-btn color="primary" @click="addSprint('Hi', 10)"> Add Sprint </v-btn>
       </v-card-actions>
       <v-divider></v-divider>
