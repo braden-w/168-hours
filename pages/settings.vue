2ad2d98e89faf8884e554d8a2f468238bef5be51
Braden <bmw02002turbo@gmail.com>
2021-01-28 21:00:37 -0800

    Add draggables

--- pages/settings.vue
+++ pages/settings.vue
@@ -2,6 +2,11 @@
   <v-container>
     <v-card>
       <v-card-title>Settings</v-card-title>
+      <draggable v-model="sprints" draggable=".item">
+        <v-card v-for="sprint in sprints" :key="sprint">
+          {{ sprint.name }}
+        </v-card>
+      </draggable>
     </v-card>
   </v-container>
 </template>
