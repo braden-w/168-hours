33002096484c73071192125f408d00bb7cfbf2d3
Braden <bmw02002turbo@gmail.com>
2021-01-28 20:43:29 -0800

    Templated Settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,19 +1,14 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-card>
+      <v-card-title>Settings</v-card-title>
+      <v-card-text></v-card-text>
+    </v-card>
+  </v-container>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
