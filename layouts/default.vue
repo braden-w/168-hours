12b2759ef55e283043e4a151baec1e7ee3c00654
Braden <bmw02002turbo@gmail.com>
2021-01-28 20:27:37 -0800

    Modify routing

--- layouts/default.vue
+++ layouts/default.vue
@@ -72,8 +72,8 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'Login',
+          to: '/login'
         }
       ],
       right: true,
