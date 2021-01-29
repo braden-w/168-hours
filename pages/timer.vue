32e54f8593c0393aaeb1e26b37b1913a3c51920b
Braden <bmw02002turbo@gmail.com>
2021-01-29 15:50:07 -0800

    Created timer.vue

---
+++ pages/timer.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>Settings</v-card-title>
+      <v-card-actions>
+        <v-btn color="success" @click="startTimer">Start timer</v-btn>
+        <v-btn color="primary" @click="addSprint('Hi', 10)"> Add Sprint </v-btn>
+      </v-card-actions>
+      <v-divider></v-divider>
+      <v-card-text>
+        <draggable
+          :list="sprints"
+          class="list-group"
+          draggable=".item"
+          @start="dragging = true"
+          @end="dragging = false"
+        >
+          <v-container
+            v-for="(sprint, index) in sprints"
+            :key="index"
+            class="item"
+          >
+            <v-card color="secondary" class="pa-3">
+              <v-row>
+                <v-col cols="9">
+                  <v-text-field
+                    v-model="sprint.name"
+                    label="Name"
+                    prepend-icon="mdi-cog"
+                  >
+                  </v-text-field>
+                </v-col>
+                <v-col cols="3">
+                  <v-text-field
+                    v-model="sprint.duration"
+                    type="number"
+                    label="Duration"
+                  >
+                  </v-text-field
+                ></v-col>
+              </v-row>
+            </v-card>
+          </v-container>
+        </draggable>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      sprints: [
+        { name: 'Work', duration: '10' },
+        { name: 'Work2', duration: '10' },
+        { name: 'Work3', duration: '10' },
+      ],
+      currentSession: { start: 0, end: 0 },
+      sessions: [],
+    }
+  },
+  methods: {
+    addSprint(name, duration) {
+      this.sprints.push({ name, duration })
+    },
+    startTimer() {
+      this.currentSession.start = new Date().getTime()
+    },
+    endTimer() {
+      this.currentSession.end = new Date().getTime()
+    },
+  },
+}
+</script>
+
+<style></style>
