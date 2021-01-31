05ef1a4997e890b2a4388ad2742ee21a1c5c0305
Braden <bmw02002turbo@gmail.com>
2021-01-30 20:19:33 -0800

    fix: Remove sprints to timer.js

--- store/index.js
+++ store/index.js
@@ -1,8 +1,4 @@
 export const state = () => ({
-  sprints: [
-    { name: 'Work', duration: 0.05, progress: 0 },
-    { name: 'Break', duration: 0.05, progress: 0 },
-  ],
   currentSession: {
     isRunning: false,
     startTime: 0,
