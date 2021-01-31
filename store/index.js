12d0dbf319bc22a05045d7230d40fa3738591190
Braden <bmw02002turbo@gmail.com>
2021-01-30 20:06:17 -0800

    fix: Comments

--- store/index.js
+++ store/index.js
@@ -11,34 +11,34 @@ export const state = () => ({
   sessions: [],
 })
 // export const mutations = {
-  // addSprint(state, name, duration) {
-  //   state.sprints.push({ name, duration })
-  // },
-  // startTimer(state, indexOfCurrentTimer) {
-  //   state.currentSession.startTime = new Date().getTime()
-  //   state.currentSession.isRunning = true
-  //   state.timer = setInterval(() => {
-  //     const currentTimer = state.sprints[indexOfCurrentTimer]
-  //     currentTimer.progress += state.settings.timer.interval
-  //     if (currentTimer.progress >= currentTimer.duration * 60 * 1000) {
-  //       indexOfCurrentTimer += 1
-  //       if (indexOfCurrentTimer === state.sprints.length) {
-  //         state.endTimer()
-  //         if (state.settings.timer.autoRestart) {
-  //           state.clearTimers()
-  //           state.startTimer(0)
-  //         }
-  //       }
-  //     }
-  //     console.log(prettyMilliseconds(currentTimer.progress))
-  //   }, state.settings.timer.interval)
-  // },
-  // endTimer(state) {
-  //   state.currentSession.isRunning = false
-  //   state.currentSession.endTime = new Date().getTime()
-  //   clearInterval(state.timer)
-  // },
-  // clearTimers(state) {
-  //   state.sprints.forEach((sprint) => (sprint.progress = 0))
-  // },
+// addSprint(state, name, duration) {
+//   state.sprints.push({ name, duration })
+// },
+// startTimer(state, indexOfCurrentTimer) {
+//   state.currentSession.startTime = new Date().getTime()
+//   state.currentSession.isRunning = true
+//   state.timer = setInterval(() => {
+//     const currentTimer = state.sprints[indexOfCurrentTimer]
+//     currentTimer.progress += state.settings.timer.interval
+//     if (currentTimer.progress >= currentTimer.duration * 60 * 1000) {
+//       indexOfCurrentTimer += 1
+//       if (indexOfCurrentTimer === state.sprints.length) {
+//         state.endTimer()
+//         if (state.settings.timer.autoRestart) {
+//           state.clearTimers()
+//           state.startTimer(0)
+//         }
+//       }
+//     }
+//     console.log(prettyMilliseconds(currentTimer.progress))
+//   }, state.settings.timer.interval)
+// },
+// endTimer(state) {
+//   state.currentSession.isRunning = false
+//   state.currentSession.endTime = new Date().getTime()
+//   clearInterval(state.timer)
+// },
+// clearTimers(state) {
+//   state.sprints.forEach((sprint) => (sprint.progress = 0))
+// },
 // }
