2d67d295c157bdd8b95a57f6c1f7ffb1b5f6f9b2
Braden <bmw02002turbo@gmail.com>
2021-01-31 18:08:34 -0800

    mutation: startTimer

--- store/index.ts
+++ store/index.ts
@@ -25,7 +25,9 @@ export const mutations = {
   addSprint({ sprints }: { sprints: Sprint[] }, payload: any) {
     sprints.push(payload)
   },
-  startTimer(state: any, indexOfCurrentSprint: number) {},
+  startTimer(state: any, timer: any) {
+    state.timer = timer
+  },
   increaseSprintByInterval(
     state: any,
     indexOfCurrentSprint: number,
@@ -66,24 +68,27 @@ export const actions = {
   startTimer({ state, commit }: any, indexOfCurrentSprint: number) {
     commit('toggleIsRunning')
     commit('logCurrentSession')
-    state.timer = setInterval(() => {
-      const currentSprint = state.sprints[indexOfCurrentSprint]
-      commit(
-        'increaseSprintByInterval',
-        indexOfCurrentSprint,
-        state.settings.timer.interval
-      )
-      if (currentSprint.progress >= currentSprint.duration * 60 * 1000) {
-        indexOfCurrentSprint += 1
-        if (indexOfCurrentSprint === state.sprints.length) {
-          state.endTimer()
-          if (state.settings.timer.autoRestart) {
-            state.clearTimers()
-            state.startTimer(0)
+    commit(
+      'startTimer',
+      setInterval(() => {
+        const currentSprint = state.sprints[indexOfCurrentSprint]
+        commit(
+          'increaseSprintByInterval',
+          indexOfCurrentSprint,
+          state.settings.timer.interval
+        )
+        if (currentSprint.progress >= currentSprint.duration * 60 * 1000) {
+          indexOfCurrentSprint += 1
+          if (indexOfCurrentSprint === state.sprints.length) {
+            state.endTimer()
+            if (state.settings.timer.autoRestart) {
+              state.clearTimers()
+              state.startTimer(0)
+            }
           }
         }
-      }
-    }, state.settings.timer.interval)
+      }, state.settings.timer.interval)
+    )
   },
   endTimer({ commit }: any, payload: any) {
     commit('toggleIsRunning')
