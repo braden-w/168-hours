14862b05d273869388cac5ee649b79cd6019f570
Braden <bmw02002turbo@gmail.com>
2021-02-01 17:06:36 -0800

    fix: Remove vue native notification from plugins

--- plugins/vue-native-notifications.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import VueNativeNotification from 'vue-native-notification'
-
-Vue.use(VueNativeNotification, {
-  // Automatic permission request before
-  // showing notification (default: true)
-  requestOnNotify: true,
-})
